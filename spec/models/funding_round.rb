2d82ef2209dfe4673bd212a015f18b05c3e70844
Omar Shammas <omar.shammas@gmail.com>
2015-01-13 14:51:18 -0500

    Adding scope to funding rounds that orders by most recent + tests

--- spec/models/funding_round.rb
+++ spec/models/funding_round.rb
@@ -3,4 +3,14 @@ require 'rails_helper'
 RSpec.describe FundingRound, type: :model do
   it { should belong_to(:company) }
   it { should have_and_belong_to_many(:investors) }
+
+  describe 'scopes' do
+    it 'orders funding rounds by the most recent' do
+      f1 = FactoryGirl.create :funding_round, announced_on: 1.month.ago
+      f2 = FactoryGirl.create :funding_round, announced_on: 1.year.ago
+      f3 = FactoryGirl.create :funding_round, announced_on: 1.day.ago
+
+      expect(FundingRound.order_by_most_recent.pluck(:id)).to eq([f3.id, f1.id, f2.id])
+    end
+  end
 end
