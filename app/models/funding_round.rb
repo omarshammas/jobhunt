de187d53b5664ea6f7386b1a59590378860a1e56
Omar Shammas <omar.shammas@gmail.com>
2015-01-13 14:51:41 -0500

    Opens company homepage url in a new page from the company show page

--- app/models/funding_round.rb
+++ app/models/funding_round.rb
@@ -1,4 +1,6 @@
 class FundingRound < ActiveRecord::Base
   belongs_to :company
   has_and_belongs_to_many :investors, -> { order(:name) }
+
+  scope :order_by_most_recent, -> { order(announced_on: :desc) }
 end
