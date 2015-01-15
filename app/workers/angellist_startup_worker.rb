f4b6a49838fd1040edfe25c9e7b67d1193ff25ad
Omar Shammas <omar.shammas@gmail.com>
2015-01-15 18:05:48 -0500

    Adding angellist startup worker to grab supplementary info to the crunchbase worker.

--- app/workers/angellist_startup_worker.rb
+++ app/workers/angellist_startup_worker.rb
@@ -2,17 +2,26 @@ class AngellistStartupWorker
   include Sidekiq::Worker
 
   def perform(company_name)
+    company = Company.find_by(name: company_name)
+    # if it does not exist, then why are we pulling in the angellist data
+    return unless company
 
+
+    company_slug = company_name.downcase.gsub(' ', '-')
     client = AngellistApi::Client.new
-    startups = client.startup_search slug: company_name
+    angl_companies = client.startup_search slug: company_slug
 
-    byebug
-    startup_id = startups.first.id
-    startup = client.get_startup startup_id
+    angl_company_id = angl_companies.id
+    angl_company = client.get_startup angl_company_id
 
-    byebug
-    p "hello"
+    # angellist quality attribute? - quality is an integer between 0 and 10, calculated every 48 hours, and reflects the company's rank on AngelList. Higher numbers mean better quality.
+    %w{ logo_url thumb_url crunchbase_url
+        angellist_quality
+    }.each do |attribute|
+      company.send("#{attribute}=", angl_company.send(attribute)) rescue nil
+    end
 
+    company.save!
   end
 
 end
--- spec/workers/angellist_startup_worker_spec.rb
+++ spec/workers/angellist_startup_worker_spec.rb
@@ -2,12 +2,25 @@ require 'rails_helper'
 
 RSpec.describe AngellistStartupWorker do
 
+  it 'if the company does not exist then does not supplement with data from angelist api' do
+    worker = AngellistStartupWorker.new
+    worker.perform('Expect Labs')
+
+    # nothing gets added
+    expect(Company.count).to eq(0)
+    #expect(Job.count).to eq(0)
+  end
+
   it 'should retrieve the startup data using the angellist api' do
+    company = FactoryGirl.create :company, name: 'Expect Labs'
     VCR.use_cassette('angellist/company-expect-labs') do
       worker = AngellistStartupWorker.new
       worker.perform('Expect Labs')
     end
 
-    # TODO
+    company.reload
+    expect(company.crunchbase_url).to eq('http://www.crunchbase.com/company/expect-labs')
+    expect(company.logo_url).to eq('https://d1qb2nb5cznatu.cloudfront.net/startups/i/39141-2a03ea2dda37a310d0226b4702a1069a-medium_jpg.jpg?buster=1349907202')
+    expect(company.thumb_url).to eq('https://d1qb2nb5cznatu.cloudfront.net/startups/i/39141-2a03ea2dda37a310d0226b4702a1069a-thumb_jpg.jpg?buster=1349907202')
   end
 end
